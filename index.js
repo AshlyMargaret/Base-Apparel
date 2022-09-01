2d84dd7ec57d3a575a65b0ad16c9da3939388a68
Ashly Margaret <ashlymargaret030@gmail.com>
2022-09-01 23:16:45 +0530

    Third Commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let emaill = document.getElementById("email_id");
 let icon__error = document.getElementById("error-icon");
 let text__error = document.getElementById("error_text");
+let sucess_msg = document.getElementById("sucess_text");
 let submit_btn = document.getElementById("submit-btn");
 let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
@@ -12,18 +13,19 @@ submit_btn.addEventListener("click",validateEmail);
 
 function validateEmail(){
 
-   if(email_value.match(validRegex)){
-    alert("Valid email address!");
-    return true;
+   if(email_value.match(validRegex)){ 
+   
+     sucess_msg.style.display ="inline-block";
+     sucess_msg.style.color = "green";
+     sucess_msg.innerText = "Thank you for subsribing to our newsletter!";
+   
+     
    }
 
    else{
-
     icon__error.style.display ="inline-block";
     text__error.style.display = "inline-block";
-
-    return false;
-
+   
 
    }
 
